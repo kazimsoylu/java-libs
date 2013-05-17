d7e055bbf14f542ec720cabfb7015d22ef9ed24d
acer <acer@acer-PC>
2013-05-17 23:12:52 +0300

    a

--- common-utils/common-utils/src/main/java/com/kazimsoylu/utils/date/DateConverter.java
+++ common-utils/common-utils/src/main/java/com/kazimsoylu/utils/date/DateConverter.java
@@ -6,8 +6,19 @@ import java.util.Date;
 
 import com.kazimsoylu.utils.exceptions.date.IllegalDateStringException;
 
+/**
+ * 
+ * @author Kazim SOYLU
+ * @since 17.05.2013
+ */
 public class DateConverter 
 {
+	/**
+	 * Converts given date string to the Date object. 
+	 * @param dateString String representation of the date
+	 * @param format Date format
+	 * @return Date object
+	 */
 	public static Date toDate( String dateString, String format )
 	{
 		SimpleDateFormat df = new SimpleDateFormat(format);
--- common-utils/common-utils/src/main/java/com/kazimsoylu/utils/date/DateFormats.java
+++ common-utils/common-utils/src/main/java/com/kazimsoylu/utils/date/DateFormats.java
@@ -3,4 +3,5 @@ package com.kazimsoylu.utils.date;
 public interface DateFormats 
 {
 	public static final String DD_MM_YYYY = "DD-MM-YYYY"; 
+
 }
